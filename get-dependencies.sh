8655ad2eac17607f469c5ea50e8c4939de9f5637
Link4Electronics <link4electronics@gmail.com>
2026-03-31 01:21:43 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,11 +9,13 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     audacious         \
     audacious-plugins \
-    fluidsynth
+    fluidsynth        \
+    pipewire-audio    \
+    pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
